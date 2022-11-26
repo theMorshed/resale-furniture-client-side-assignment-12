ec5ea8b86d749e032ae63d5ca4df104fa9052d32
theMorshed <themorshedctg@gmail.com>
2022-11-26 09:38:24 +0600

    in the dashboard admin will see all sellers and all buyers component ready with api connection

--- src/Pages/Dashboard/AllBuyers.js
+++ src/Pages/Dashboard/AllBuyers.js
@@ -1,9 +1,34 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import LoadingSpinner from '../../Sections/Common/LoadingSpinner';
 
 const AllBuyers = () => {
+
+    const {data: allBuyers, isLoading} = useQuery({
+        queryKey: ['buyers'],
+        queryFn: async () => {
+            const result = await fetch('http://localhost:5000/allbuyers');
+            const data = await result.json();
+            return data;
+        }
+    });
+
+    if (isLoading) {
+        return <LoadingSpinner></LoadingSpinner>
+    }
+    
     return (
         <div>
-            All Buyers are present here.
+            <h2 className="text-3xl font-bold text-gray-500 mb-10">All Buyers:</h2>
+
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-6'>
+                {
+                    allBuyers.map(buyer => <div className='border p-3 rounded-lg hover:border-primary' key={buyer._id}>
+                        <p className="text-gray-500">Name: {buyer.name}</p>
+                        <button className="btn btn-primary mt-6">Delete</button>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
--- src/Pages/Dashboard/AllSellers.js
+++ src/Pages/Dashboard/AllSellers.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import SellerBuyers from './SellerBuyers';
 
 const AllSellers = () => {
+    const [allSellers, setAllSellers] = useState([]);
+    useEffect(() => {
+        axios('http://localhost:5000/allsellers')
+            .then(result => {
+                setAllSellers(result.data);
+            });
+    }, []);
+    
     return (
         <div>
-            All sellers are here.
+            <h2 className="text-3xl font-bold text-gray-500 mb-10">All Sellers:</h2>
+
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-6'>
+                {
+                    allSellers.map(seller => <div className='border p-3 rounded-lg hover:border-primary' key={seller._id}>
+                        <p className="text-gray-500">Name: { seller.name }</p>
+                        <button className="btn btn-primary mt-6">Delete</button>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
